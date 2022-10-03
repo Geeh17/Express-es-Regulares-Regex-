e2769c511b8c244f49ca51e27ef38d1b09aa0b92
Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>
2022-10-03 00:15:22 -0300

    Create ProblemaPonto.js
    Co-Authored-By: Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>

---
+++ Caracteres/ProblemaPonto.js
@@ -0,0 +1,4 @@
+const texto = 'Bom\ndia'
+console.log(texto.match(/.../gi))
+console.log(texto.match(/..../gi)) //o ponto não engloba o \n
+// dotall - algumas linguagens tem um flag /exp/s, mais JS não!
