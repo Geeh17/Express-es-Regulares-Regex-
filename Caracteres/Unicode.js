5fb7dbbfb1a77b7f5677f4da5a717068109a6996
Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>
2022-10-03 00:15:20 -0300

    Create Unicode.js
    Co-Authored-By: Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>

---
+++ Caracteres/Unicode.js
@@ -0,0 +1,15 @@
+
+//no inicio...
+//Um byte (8 bits) - 256 caracteres
+//Simbolos, pontuação, A-Z, a-z, 0-9
+
+//Dois bytes (16 bits) - 6500+ caracteres
+//+ simbolos, +pontuação, A-Z, a-z, 0-9
+
+//Unicode
+//Quantidade de bytes variavel - expansível
+//suporta mais de 1 milhão de caracteres
+//Atualmente tem mais de 100.00 caracteres atribuidos
+
+// https://unicode-table.com/pt/
+
